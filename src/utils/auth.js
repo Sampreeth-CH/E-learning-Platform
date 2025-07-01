f1c5a08e158fe7a4723d0bfc57d51af35a39751a
Sampreeth C H <sampreethchsampreethch@gmail.com>
2025-07-01 13:24:09 +0530

    Fixed Home route and routing issue

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,16 +1,13 @@
-// utils/auth.js
-
 const USERS_KEY = 'users';
 const LOGGED_IN_USER_KEY = 'loggedInUser';
 
-// Save user to localStorage
 export const saveUser = (user) => {
   const users = JSON.parse(localStorage.getItem(USERS_KEY)) || [];
   users.push(user);
   localStorage.setItem(USERS_KEY, JSON.stringify(users));
 };
 
-// Authenticate user
+
 export const loginUser = (email, password) => {
   const users = JSON.parse(localStorage.getItem(USERS_KEY)) || [];
   const matchedUser = users.find((u) => u.email === email && u.password === password);
@@ -23,12 +20,12 @@ export const loginUser = (email, password) => {
   return null;
 };
 
-// Logout
+
 export const logoutUser = () => {
   localStorage.removeItem(LOGGED_IN_USER_KEY);
 };
 
-// Get current logged in user
+
 export const getUser = () => {
   const user = localStorage.getItem(LOGGED_IN_USER_KEY);
   return user ? JSON.parse(user) : null;
